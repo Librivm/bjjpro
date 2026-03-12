befa822c492af69cdfcc36d016992f8ada6e2895
Librivm <luke.roberts.librivm@gmail.com>
2026-03-12 15:53:46 +1300

    SecurityUpdate4

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import { createClient } from "@supabase/supabase-js";
 //                              WITH CHECK (user_id = auth.uid())
 // ─────────────────────────────────────────────────────────────────────────────
 const SUPABASE_URL     = import.meta.env.VITE_SUPABASE_URL     || "";
-const SUPABASE_ANON_KEY= import.meta.env.VITE_SUPABASE_ANON_KEY|| "";|| "";
+const SUPABASE_ANON_KEY= import.meta.env.VITE_SUPABASE_ANON_KEY|| "";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
